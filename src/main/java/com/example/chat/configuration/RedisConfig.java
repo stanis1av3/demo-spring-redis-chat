68899bd08b51fc29fa3ac6fdfc96b11ce5395871
Pavlovskiy <gluckboy@gmail.com>
2018-08-06 22:51:54 +0300

    init

--- src/main/java/com/example/chat/configuration/RedisConfig.java
+++ src/main/java/com/example/chat/configuration/RedisConfig.java
@@ -14,6 +14,7 @@ import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer
 @Configuration
 public class RedisConfig {
 
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         JedisConnectionFactory jedisConFactory
